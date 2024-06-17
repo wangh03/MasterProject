aa01e9a00ec97fafc03bb84565b3a9e7f6f39cf5
Carl Fredrik Berg <carlpaatur@hotmail.com>
2024-06-17 13:46:25 +0200

    Update run_example_Python.sh

--- examples/Flow/CO2JointOpt2w/run_example_Python.sh
+++ examples/Flow/CO2JointOpt2w/run_example_Python.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# This is a bash script for running the example with two algorithm.
+# This is a bash script for running the example with external
+# NPV calculations. This example uses two different 
+# optimization algorithms.
 
 # Check if the user provided two parameters.
 # -----------------------------------------------------------
@@ -7,26 +9,26 @@ if [ $# -ne 2 ]; then
     echo "Usage: $0 execution help "
     echo "agr1:<ALGO_ABBR> the algorithm abbreviation to run. GA/PSO"
     echo "agr2:<PYTHON_DIR> the python directory includes .py"
-    exit 1 # indicate failing to run
+    exit 1 # indicates a failed run
 fi
 
 # Store the parameters provided by the user.
 # ------------------------------------------------------------
-ALGO_ABBR="$1" # the optimization algorithm : GA,PSO.
-REA_PY_DIR="$2" # the relative python script path that will be executed for external objective function calculation.
+ALGO_ABBR="$1" # the optimization algorithm: GA or PSO.
+REA_PY_DIR="$2" # the relative Python script path that will be executed for the external objective function calculation.
 
 # ---------------------------------------------------------
-# store the path variables for simulator/FieldOpt/python interpreter.
+# store the path variables for the simulator/FieldOpt/python interpreter.
 
-FIELDOPT_BIN=FieldOpt # this line need to link FieldOpt form build directory to bin.
+FIELDOPT_BIN=FieldOpt # this line gives the directory for FieldOpt.
 CURRENT_DIR=$(pwd) # get the present working directory.
-FIELDOPT_OUT=${CURRENT_DIR}/Output # the directory to store optimization result.
-MODEL_DIR=${CURRENT_DIR}/CO2OPT2W # the directory of base case model.
+FIELDOPT_OUT=${CURRENT_DIR}/Output # the directory to store the optimization results.
+MODEL_DIR=${CURRENT_DIR}/CO2OPT2W # the directory of the base case model.
 DECK_PATH=${MODEL_DIR}/CO2OPT2W.DATA # .DATA file path of the base case model.
 GRID_PATH=${MODEL_DIR}/CO2OPT2W.EGRID # .EGRID file path of the base case model.
 OUTPUT_DIR=${FIELDOPT_OUT}/${ALGO_ABBR}
 UNSMARY_DIR=${OUTPUT_DIR}/CO2OPT2W
-ABS_PY_DIR=${CURRENT_DIR}/${REA_PY_DIR} # abosulute python script directiory.
+ABS_PY_DIR=${CURRENT_DIR}/${REA_PY_DIR} # abosulute python script directory.
 JSON_DRIVER_FILE=${CURRENT_DIR}/fo_driver.CO2OPT.${ALGO_ABBR}.json
 
 #-----------------------------------------------------------
@@ -38,7 +40,7 @@ py_exec_str="/usr/bin/python3 "${ABS_PY_DIR}"ObjFunCase.py \"${UNSMARY_DIR}\" \"
 
 rm -f bash_flw_py_"${ALGO_ABBR}".sh
 
-# append the python execution string to bash file of FieldOpt input
+# append the Python execution string to the bash file for the FieldOpt input
 sed -e '$a\'$'\n'"${py_exec_str}" base_bash.sh > bash_flw_py_"${ALGO_ABBR}".sh
 
 chmod +x bash_flw_py_"${ALGO_ABBR}".sh
